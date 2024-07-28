10417abbf97bf2dd9b6972604822ecdcb6ef865c
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-07-29 02:05:17 +0500

    RU: Oscallo.Castle.AddonedKernel обнолвен до версии 0.9.2. Созданы: 1. Заготовки под Injector. 2. Реализован интерфейс IHaveDependencies со статическим методом ReginsterDependencies.
    EN: Oscallo.Castle.AddonedKernel has been updated to version 0.9.2.
    Created:
    1. Blanks for Injector.
    2. Implemented the IHaveDependencies interface with the static ReginsterDependencies method.

---
+++ src/DataServer.Core.Demo/DI/IntegrableInterfaces/IHaveDependencies.cs
@@ -0,0 +1,9 @@
+﻿using Oscallo.Castle.AddonedKernel.Injectors;
+
+namespace DataServer.Core.Demo.DI.IntegrableInterfaces
+{
+    internal interface IHaveDependencies
+    {
+        public static abstract void ReginsterDependencies(IRegistrar registrar);
+    }
+}
