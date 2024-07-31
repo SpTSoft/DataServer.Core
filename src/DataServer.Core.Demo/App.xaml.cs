e5445d727ed32df623831217db7544db205fc1be
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-08-01 00:28:12 +0500

    Запуск gatewayListener для тестирования/Running gatewayListener for testing

--- src/DataServer.Core.Demo/App.xaml.cs
+++ src/DataServer.Core.Demo/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Castle.MicroKernel.Registration;
+﻿using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using DataServer.Core.Demo.DI;
+using DataServer.Core.Net;
 using Oscallo.Castle.AddonedKernel.Injectors;
+using System.Net;
 using System.Windows;
 
 namespace DataServer.Core.Demo
@@ -11,12 +14,27 @@ namespace DataServer.Core.Demo
     /// </summary>
     public partial class App : Application
     {
+		private readonly IInjector _Injector;
+
         public App() 
         {
             WindsorContainer container = new();
 
             Injector.Register(container, Component.For<IWindsorContainer>().Instance(container).LifeStyle.Singleton);
             Injector.Register(container, Component.For<IInjector>().ImplementedBy<Injector>().LifeStyle.Singleton);
-        }
+
+			this._Injector = Injector.Resolve<IInjector>(container);
+
+			this._Injector.RegisterIfAbsent<IGatewayListener>(Component.For<IGatewayListener>().ImplementedBy<GatewayListener>().LifeStyle.Singleton);
+
+			Arguments args = new()
+			{
+				{ "iPAddress", IPAddress.Parse("127.0.0.1") }, {"Port", NetHelper.GetAvailablePort() }
+			};
+
+			IGatewayListener gatewayListener = this._Injector.Resolve<IGatewayListener>(args);
+
+			gatewayListener.Run();
+		}
     }
 }
