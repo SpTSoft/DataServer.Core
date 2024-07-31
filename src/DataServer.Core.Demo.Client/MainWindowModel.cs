227325e478aac630df6021a7724ad19c9d0878c6
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-08-01 00:25:33 +0500

    Добавлен демонстрационный клиент для тестирования сетевого взаимодействия/Added demo client for testing network interaction

---
+++ src/DataServer.Core.Demo.Client/MainWindowModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace DataServer.Core.Demo.Client
+{
+	public class MainWindowModel 
+	{
+		public int Port { get; set; }
+
+		public IPAddress IPAddress { get; set; } = IPAddress.Any;
+
+		public string Log { get; set; } = string.Empty;
+	}
+}
---
+++ src/DataServer.Core.Demo.Client/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DataServer.Core.Demo.Client
+{
+	public class RelayCommand : ICommand
+	{
+		private readonly Action<object?> _Execute;
+		private readonly Func<object?, bool>? _CanExecute;
+
+		event EventHandler? ICommand.CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+		{
+			this._Execute = execute;
+			this._CanExecute = canExecute;
+		}
+
+		bool ICommand.CanExecute(object? parameter) { return this._CanExecute == null || this._CanExecute(parameter); }
+
+		void ICommand.Execute(object? parameter) { this._Execute(parameter); }
+	}
+}
