0f6e7ee0200b00957b8d11e849531072e1fcccde
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2024-08-02 00:41:52 +0500

    Сделано представление для сервера/Presentation made for server

---
+++ src/DataServer.Core.Demo/MainMVVM/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DataServer.Core.Net;
+using System;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace DataServer.Core.Demo.MainMVVM
+{
+	public class MainViewModel : IMainViewModel
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+		public event PropertyChangingEventHandler? PropertyChanging;
+
+		private IGatewayListener _GatewayListener;
+
+		public MainViewModel(IGatewayListener gatewayListener)
+		{
+			this._GatewayListener = gatewayListener;
+		}
+
+
+		public int Port
+		{
+			get { return this._GatewayListener.Port; }
+		}
+
+		public IPAddress IPAddress
+		{
+			get { return this._GatewayListener.IPAddress; }
+		}
+
+		public void OnPropertyChanged([CallerMemberName] string? prop = "")
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+
+		public void OnPropertyChanging([CallerMemberName] string? prop = "")
+		{
+			this.PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(prop));
+		}
+	}
+}
---
+++ src/DataServer.Core.Demo/MainMVVM/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using DataServer.Core.Demo.DI;
+using System.Windows;
+
+namespace DataServer.Core.Demo.MainMVVM
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, IMainWindow
+	{
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+		public MainWindow(IMainViewModel mainViewModel)
+		{
+			InitializeComponent();
+			this.DataContext = mainViewModel;
+		}
+	}
+}
